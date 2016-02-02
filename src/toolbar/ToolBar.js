7048a584ee2c121e58e676763349154b73a3f4eb
dme <denis.meier1@gmail.com>
2016-02-02 14:07:01 +0100

    [update] col from action buttons+

--- src/toolbar/ToolBar.js
+++ src/toolbar/ToolBar.js
@@ -151,7 +151,7 @@ class ToolBar extends React.Component{
 
     return(
       <div className="row">
-        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-8">
+        <div className="col-xs-1">
           <div className="btn-group btn-group-sm" role="group">
             {exportCSV}
             {insertBtn}
